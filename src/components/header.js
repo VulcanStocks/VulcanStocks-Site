73cf200203416b509228839b5beadc56f5a2daa1
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-08-25 19:15:46 +0200

    router fixed

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header>
       <img src='https://i.postimg.cc/HW75tyLP/Vulcan-stocks-PS-calibri-bakgrund.png' alt='' />
-        <Link to="/" style={{ textDecoration: 'none' }}><h1>Home</h1></Link> |{" "}
+        <Link to="/" style={{ textDecoration: 'none' }}><h1>Home</h1></Link>
         <Link to="/software" style={{ textDecoration: 'none' }}><h1>Software</h1></Link>
         <Link to="/equityResearch" style={{ textDecoration: 'none' }}><h1>Equity Research</h1></Link>
         <Link to="/technicalAnalysis" style={{ textDecoration: 'none' }}><h1>Technical Analysis</h1></Link>
